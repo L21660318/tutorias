2ab7b159c80755d22509ce6a9f182bee78c9141a
L21660318 <l21660318@matehuala.tecnm.mx>
2025-10-15 22:24:38 -0600

    Update settings.py

--- pit_sistema/settings.py
+++ pit_sistema/settings.py
@@ -96,6 +96,7 @@ DATABASES = {
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
             'charset': 'utf8mb4',
+            'ssl': {'ssl-mode': 'REQUIRED'},
         },
     }
 }
